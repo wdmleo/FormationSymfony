c6d3b45c0999f1ee4985a23eb335f5fda693e00b
wdmleo <leo@wdmstudio.fr>
2018-09-13 11:02:23 +0200

    Entity

--- J4_EntityEtForm/src/AppBundle/Controller/DoctController.php
+++ J4_EntityEtForm/src/AppBundle/Controller/DoctController.php
@@ -7,7 +7,60 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+// use AppBundle\Entity\Message;
+
+/**
+ * @Route("/doct")
+ */
 class DoctController extends Controller
 {
-    
+    /**
+     * @Route("/all", name="doct_all")
+     */
+    public function allAction(Request $request) {
+        $em = $this->getDoctrine()->getManager();
+
+        $messages = $em->getRepository("AppBundle:Message")->findAll();
+        return $this->render('default/twig_home.html.twig', [
+            'messages'=>$messages
+        ]);
+    }
+
+    /**
+     * @Route("/init", name="doct_init")
+     */
+    public function initAction(Request $request) {
+        $em = $this->getDoctrine()->getManager();
+
+        // $message = new Message();
+        $message = new \AppBundle\Entity\Message();
+        $message->setTitre('Mon super message');
+        $message->setMessage('fjekfj kwajf wajod jwad wao djwadw hdwajdhwa djwad iawdjwadi;wa dwad iw fiwaifwaifowa fwa fowa fuow fupow ufpwa');
+        $message->setDate(new \DateTime());
+        $message->setDestinataire("johnsmith");
+
+        $em->persist($message);
+
+        // $em->clear();
+
+        $em->flush();
+        return $this->redirectToRoute('doct_home');
+    }
+
+    /**
+     * @Route("/{destinataire}", name="doct_home")
+     */
+    public function homeAction(Request $request, $destinataire) {
+        $em = $this->getDoctrine()->getManager();
+
+        $repository = $em->getRepository('AppBundle:Message');
+        $msgId1 = $repository->find(1);
+
+        $msgId1 = $repository->findOneByDestinataire($destinataire);
+        // SELECT * FROM message WHERE destinataire = $destinataire
+
+        return $this->render('default/twig_home.html.twig', [
+            'message'=>$msgId1
+        ]);
+    }
 }
---
+++ J4_EntityEtForm/src/AppBundle/Entity/Message.php
@@ -0,0 +1,158 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Message
+ *
+ * @ORM\Table(name="message")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\MessageRepository")
+ */
+class Message
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="titre", type="string", length=255)
+     */
+    private $titre;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="message", type="text")
+     */
+    private $message;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="date", type="datetime")
+     */
+    private $date;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="destinataire", type="string", length=255)
+     */
+    private $destinataire;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set titre
+     *
+     * @param string $titre
+     *
+     * @return Message
+     */
+    public function setTitre($titre)
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    /**
+     * Get titre
+     *
+     * @return string
+     */
+    public function getTitre()
+    {
+        return $this->titre;
+    }
+
+    /**
+     * Set message
+     *
+     * @param string $message
+     *
+     * @return Message
+     */
+    public function setMessage($message)
+    {
+        $this->message = $message;
+
+        return $this;
+    }
+
+    /**
+     * Get message
+     *
+     * @return string
+     */
+    public function getMessage()
+    {
+        return $this->message;
+    }
+
+    /**
+     * Set date
+     *
+     * @param \DateTime $date
+     *
+     * @return Message
+     */
+    public function setDate($date)
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    /**
+     * Get date
+     *
+     * @return \DateTime
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    /**
+     * Set destinataire
+     *
+     * @param string $destinataire
+     *
+     * @return Message
+     */
+    public function setDestinataire($destinataire)
+    {
+        $this->destinataire = $destinataire;
+
+        return $this;
+    }
+
+    /**
+     * Get destinataire
+     *
+     * @return string
+     */
+    public function getDestinataire()
+    {
+        return $this->destinataire;
+    }
+}
