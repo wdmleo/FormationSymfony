8e52b882919b1dba18946c35c870b23701f727b5
wdmleo <leo@wdmstudio.fr>
2018-09-13 10:07:57 +0200

    Ajout controller

---
+++ J4_EntityEtForm/src/AppBundle/Controller/DoctController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DoctController extends Controller
+{
+    
+}
